1f66b260901d6936e9491254aa9a79e00650b347
Jackson452261 <a411758@gmail.com>
2025-01-14 15:22:34 +0800

    更改project網站

--- mail.php
+++ mail.php
@@ -1,5 +1,5 @@
 <?php
-$subject    = 'E-mail from example'; // Subject of your email
+$subject    = 'E-mail from example';  
 $to         = 'e-mail@a411757@gmail.com'; //Your e-mail address
 $headers    = 'MIME-Version: 1.0' . "\r\n" .
               'Content-type: text/html; charset=iso-8859-1' . "\r\n";
